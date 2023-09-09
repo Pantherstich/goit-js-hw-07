44fadb950108e6495f6d547c51b97db72159a444
Pantherstich <pantherstich@gmail.com>
2023-09-09 16:54:18 +0300

    start

--- js/task-07.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
-input.addEventListener("input", fontSize);
-
-text.style.fontSize = `${input.value}px`;
-
-function fontSize(event) {
-    text.style.fontSize = `${event.currentTarget.value}px`;
-}
